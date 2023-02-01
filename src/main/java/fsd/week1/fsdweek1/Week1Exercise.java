bf4cc8ace5064e4242e44ed88be5de9d2415eb15
Gareth Shippen <100131636+garethshippen@users.noreply.github.com>
2023-02-01 14:13:27 +0000

    Exercise 2 with whistles done

--- src/main/java/fsd/week1/fsdweek1/Week1Exercise.java
+++ src/main/java/fsd/week1/fsdweek1/Week1Exercise.java
@@ -13,7 +13,7 @@ public class Week1Exercise extends Application
     public void start(Stage stage) throws IOException
     {
         FXMLLoader fxmlLoader = new FXMLLoader(Week1Exercise.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("First JavaFX Application");
         stage.setScene(scene);
         stage.show();
